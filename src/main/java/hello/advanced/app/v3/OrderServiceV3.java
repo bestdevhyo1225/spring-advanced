775ddf060b6ad1cf3a1239f6d70c7548ef51c9de
janghyoseog <bestdevhyo1225@gmail.com>
2022-03-27 17:22:11 +0900

    feat: 필드 동기화 - 적용

---
+++ src/main/java/hello/advanced/app/v3/OrderServiceV3.java
@@ -0,0 +1,26 @@
+package hello.advanced.app.v3;
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.logtrace.LogTrace;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV3 {
+
+    private final OrderRepositoryV3 orderRepository;
+    private final LogTrace trace;
+
+    public void orderItem(String itemId) {
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderService.orderItem()");
+            orderRepository.save(itemId);
+            trace.end(status);
+        } catch (Exception exception) {
+            trace.exception(status, exception);
+            throw exception; // 예외를 꼭 다시 던져주어야 한다.
+        }
+    }
+}
