c17a793aeff736e591d6ae844294750abc97a87b
janghyoseog <bestdevhyo1225@gmail.com>
2022-03-27 16:37:52 +0900

    feat: 로그 추적기 V2 - 파라미터로 동기화 개발

---
+++ src/main/java/hello/advanced/trace/hellotrace/HelloTraceV2.java
@@ -0,0 +1,77 @@
+package hello.advanced.trace.hellotrace;
+
+import hello.advanced.trace.TraceId;
+import hello.advanced.trace.TraceStatus;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class HelloTraceV2 {
+
+    private static final String START_PREFIX = "-->";
+    private static final String COMPLETE_PREFIX = "<--";
+    private static final String EX_PREFIX = "<X-";
+
+    public TraceStatus begin(String message) {
+        TraceId traceId = new TraceId();
+        Long startTimeMs = System.currentTimeMillis();
+        log.info("[{}] {}{}", traceId.getId(), addSpace(START_PREFIX, traceId.getLevel()), message);
+        return new TraceStatus(traceId, startTimeMs, message);
+    }
+
+    // V2에서 새롭게 추가됨
+    public TraceStatus beginSync(TraceId beforeTraceId, String message) {
+        TraceId nextId = beforeTraceId.createNextId();
+        Long startTimeMs = System.currentTimeMillis();
+        log.info("[{}] {}{}", nextId.getId(), addSpace(START_PREFIX, nextId.getLevel()), message);
+        return new TraceStatus(nextId, startTimeMs, message);
+    }
+
+    public void end(TraceStatus status) {
+        complete(status, null);
+    }
+
+    public void exception(TraceStatus status, Exception exception) {
+        complete(status, exception);
+    }
+
+    private void complete(TraceStatus status, Exception exception) {
+        Long stopTimeMs = System.currentTimeMillis();
+        long resultTimeMs = stopTimeMs - status.getStartTimeMs();
+        TraceId traceId = status.getTraceId();
+        if (exception == null) {
+            log.info(
+                "[{}] {}{} time={}ms",
+                traceId.getId(), addSpace(COMPLETE_PREFIX, traceId.getLevel()), status.getMessage(), resultTimeMs
+            );
+        } else {
+            log.info(
+                "[{}] {}{} time={}ms ex={}",
+                traceId.getId(), addSpace(EX_PREFIX, traceId.getLevel()), status.getMessage(), resultTimeMs, exception.toString()
+            );
+        }
+    }
+
+    /**
+     * ----- [ Prefix : --> ] -----
+     * level = 0
+     * level = 1, |-->
+     * level = 2, |   |-->
+     * ----- [ COMPLETE_PREFIX : <-- ] -----
+     * level = 0
+     * level = 1, |<--
+     * level = 2, |   |<--
+     * ----- [ EX_PREFIX : <X- ] -----
+     * level = 0 ex
+     * level = 1 ex, |<X-
+     * level = 2 ex, |   |<X-
+     * */
+    private static String addSpace(String prefix, int level) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < level; i++) {
+            stringBuilder.append((i == level - 1) ? "|" + prefix : "|   ");
+        }
+        return stringBuilder.toString();
+    }
+}
